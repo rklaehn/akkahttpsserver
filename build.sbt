379351e4306bfb6f334facfac09219d3b3b0068f
Rüdiger Klaehn <rklaehn@gmail.com>
2016-01-25 20:53:37 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "httpsserver"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-stream-experimental" % "2.0.2"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-http-core-experimental" % "2.0.2"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-http-experimental" % "2.0.2"
---
+++ src/main/scala/httpsserver/Server.scala
@@ -0,0 +1,47 @@
+package httpsserver
+
+import java.io.InputStream
+import java.security.{SecureRandom, KeyStore}
+import javax.net.ssl.{KeyManagerFactory, SSLContext}
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.{HttpsContext, Http}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+
+object Server extends App {
+
+  private def resourceStream(resourceName: String): InputStream = {
+    val is = getClass.getClassLoader.getResourceAsStream(resourceName)
+    require(is ne null, s"Resource $resourceName not found")
+    is
+  }
+
+  val serverContext: HttpsContext = {
+    val password = "abcdef".toCharArray
+    val context = SSLContext.getInstance("TLS")
+    val ks = KeyStore.getInstance("PKCS12")
+    ks.load(resourceStream("keys/server.p12"), password)
+    val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(ks, password)
+    context.init(keyManagerFactory.getKeyManagers, null, new SecureRandom)
+    // start up the web server
+    HttpsContext(context)
+  }
+
+  implicit val system = ActorSystem("server")
+  implicit val materializer = ActorMaterializer()
+  import system._
+
+  val route = Route(complete("ok"))
+
+  val serverSource = Http().bind(interface = "0.0.0.0", port = 8081, httpsContext = Some(serverContext))
+  val bindingFuture = serverSource.to(Sink.foreach { connection =>
+    // foreach materializes the source
+    println("Accepted new connection from " + connection.remoteAddress)
+    // ... and then actually handle the connection
+    connection.handleWith(route)
+  }).run()
+}
