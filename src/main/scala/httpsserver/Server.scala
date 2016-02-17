cd6ee5886b0cac9fa88f1d0d6fc39834678191c9
Rüdiger Klaehn <rklaehn@gmail.com>
2016-02-17 16:17:06 +0100

    Added ws-echo websocket demo
    Go to https://localhost/ws for test page

--- src/main/scala/httpsserver/Server.scala
+++ src/main/scala/httpsserver/Server.scala
@@ -4,10 +4,12 @@ import java.security.{SecureRandom, KeyStore}
 import javax.net.ssl.{KeyManagerFactory, SSLContext}
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.ws.{TextMessage, Message}
 import akka.http.scaladsl.{ConnectionContext, Http}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Flow
 
 object Server extends App {
 
@@ -23,11 +25,24 @@ object Server extends App {
     ConnectionContext.https(context)
   }
 
+  val echoService: Flow[Message, Message, _] = Flow[Message].map {
+    case TextMessage.Strict(txt) => TextMessage("ECHO: " + txt)
+    case _ => TextMessage("Message type unsupported")
+  }
+
   implicit val system = ActorSystem("server")
   implicit val materializer = ActorMaterializer()
   import system._
 
   val route: Route = Route(
+    path("ws") {
+      getFromResource("web/index.html")
+    } ~
+    path("ws-echo") {
+      get {
+        handleWebSocketMessages(echoService)
+      }
+    } ~
     complete("ok"))
 
   Http().bindAndHandle(route, interface = "0.0.0.0", port = 8081, connectionContext = serverContext)
